228ccc7969fa0c6ef7cb99db64d8af1f9a26e97c
Tambouil <v.berceaux@gmail.com>
2022-07-10 15:11:15 +0200

    feat (user) : admin and clients add form

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -3,25 +3,26 @@ import User from '../../Models/User'
 import RegisterValidator from '../../Validators/RegisterValidator'
 
 export default class AuthController {
-  public showLoginForm({ view }: HttpContextContract) {
-    return view.render('auth/login')
-  }
   public async login({ request, auth, response }: HttpContextContract) {
     const { username, password } = request.only(['username', 'password'])
 
     await auth.attempt(username, password)
     return response.redirect('/home')
   }
-  public showRegisterForm({ view }: HttpContextContract) {
-    return view.render('auth/register')
+  public async registerClient({ request, response, session }: HttpContextContract) {
+    const payload = await request.validate(RegisterValidator)
+
+    await User.create(payload)
+    session.flash('success', 'User created successfully')
+    return response.redirect().back()
   }
-  public async register({ request, auth, response }: HttpContextContract) {
+  public async registerAdmin({ request, auth, response }: HttpContextContract) {
     const payload = await request.validate(RegisterValidator)
 
     const user = await User.create(payload)
 
     await auth.login(user)
-    return response.redirect('/')
+    return response.redirect('/home')
   }
   public async logout({ auth, response }: HttpContextContract) {
     await auth.logout()
