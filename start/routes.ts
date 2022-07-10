bc553d42fae66c4c107968c92b719e00b3916ba2
Tambouil <v.berceaux@gmail.com>
2022-07-10 15:15:18 +0200

    fix: routes and paths

--- start/routes.ts
+++ start/routes.ts
@@ -22,9 +22,11 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.on('/').render('auth/login')
 Route.on('/home').render('pages/home')
+Route.on('/upload').render('pages/upload')
+Route.on('/addclient').render('auth/register-client')
+Route.on('/addadmin').render('auth/register-admin')
 
-Route.get('login', 'AuthController.showLoginForm')
 Route.post('login', 'AuthController.login')
 Route.delete('logout', 'AuthController.logout')
-Route.get('register', 'AuthController.showRegisterForm')
-Route.post('register', 'AuthController.register')
+Route.post('registerAdmin', 'AuthController.registerAdmin')
+Route.post('registerClient', 'AuthController.registerClient')
